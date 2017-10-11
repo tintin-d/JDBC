80abe9c68e5c3a14f0fe611525d368c99be43b18
Bryan Gonowree <bryangonowree@gmail.com>
2017-10-11 16:35:06 +0400

    bryantest

--- src/graphic/MAinWindow.java
+++ src/graphic/MAinWindow.java
@@ -70,7 +70,7 @@ public class MAinWindow {
 		//frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-
+		"bryantest"
 		//afficheResult();
 		afficheConnect();
 	//	removeConnectionPane();
